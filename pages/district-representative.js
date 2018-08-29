163d0e930f4d6ce34e6f8c1041f000e144261dc3
abdul ahmad <ahmadabdul3@yahoo.com>
2018-08-28 22:00:12 -0400

    update initiative styles - add new initiative

--- pages/district-representative.js
+++ pages/district-representative.js
@@ -43,6 +43,7 @@ export default class DistrictRepresentative extends PureComponent {
         <ScrollView>
           <InitiativeSummary title='Pave roads' />
           <InitiativeSummary title='Increase something' />
+          <InitiativeSummary title='Other initiative' />
         </ScrollView>
       </View>
     );
--- styles/components/initiative-summary.js
+++ styles/components/initiative-summary.js
@@ -10,6 +10,9 @@ import { cardStyles } from 'px/styles/components/card';
 const styles = StyleSheet.create({
   initiativeSummary: {
     ...cardStyles,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 5,
   },
 });
 
