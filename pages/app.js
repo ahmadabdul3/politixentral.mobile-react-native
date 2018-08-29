a6956b475084d51013125db26ef4a544ac29ce40
abdul ahmad <ahmadabdul3@yahoo.com>
2018-08-28 22:24:34 -0400

    Add icons to main app tabs

--- pages/app.js
+++ pages/app.js
@@ -4,16 +4,35 @@ import City from 'px/pages/city';
 import District from 'px/pages/district';
 import DistrictRepresentative from 'px/pages/district-representative';
 import colors from 'px/styles/colors';
+import { Ionicons, MaterialIcons, Foundation } from '@expo/vector-icons';
 
 export default createMaterialBottomTabNavigator({
   City: {
     screen: City,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => {
+        const color = focused ? 'white' : colors.logoGreenLight;
+        return <MaterialIcons name="location-city" size={23} color={color} />;
+      },
+    },
   },
   District: {
     screen: District,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => {
+        const color = focused ? 'white' : colors.logoGreenLight;
+        return <Foundation name="map" size={20} color={color} />;
+      },
+    },
   },
   Alder: {
     screen: DistrictRepresentative,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => {
+        const color = focused ? 'white' : colors.logoGreenLight;
+        return <MaterialIcons name="person-outline" size={24} color={color} />;
+      },
+    },
   },
 }, {
   tabBarPosition: 'bottom',
