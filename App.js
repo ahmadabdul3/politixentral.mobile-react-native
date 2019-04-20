80cc23e5c4c3f9744389f6adb28ddb66fdcc5dfb
abdul ahmad <ahmadabdul3@yahoo.com>
2019-04-20 17:04:24 -0400

    Add push notifications

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Notifications } from 'expo';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Router from 'px/router';
 import { Provider } from 'react-redux';
 import reducer from 'px/redux';
 import thunk from 'redux-thunk';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { requestPermissionForPushNotifications } from 'px/services/push_notification_permissions';
-
-requestPermissionForPushNotifications();
 
 const store = createStore(
   reducer, // new root reducer with router state
@@ -18,6 +17,42 @@ const store = createStore(
 );
 
 export default class App extends React.Component {
+  state = {
+    notification: {},
+  };
+
+  componentDidMount() {
+    requestPermissionForPushNotifications();
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
+  }
+
+  _handleNotification = (notification) => {
+    console.log('notification', notification);
+    if (notification.origin === 'received') {
+      Alert.alert(
+        'New Message',
+        notification.data.message,
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    }
+    this.setState({ notification: notification });
+  };
+
+  // render() {
+  //   return (
+  //     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  //       <Text>Origin: {this.state.notification.origin}</Text>
+  //       <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+  //     </View>
+  //   );
+  // }
+
   render() {
     return (
       <Provider store={store}>
